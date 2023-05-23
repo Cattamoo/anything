35d80030e1cc83edab8b6a89d8fe845d6c2a3cde
Cattamoo <ouob77@kakao.com>
2023-05-24 00:24:38 +0900

    router 설정 및 각 페이지 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
 import App from './App';
+import Home from "./pages/Home";
+import Board from "./pages/Board";
+import Post from "./pages/Post";
+
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <App />,
+			children: [
+				{ index: true, element: <Home /> },
+				{ path: '/board/:id', element: <Board /> },
+				{ path: '/post/:id', element: <Post /> },
+			]
+		}
+	]
+)
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
@@ -10,7 +30,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
 
