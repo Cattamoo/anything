a993514d644dea4a195603fd45eaf0b56840f894
Cattamoo <ouob77@kakao.com>
2023-05-24 22:21:58 +0900

    Post 새로고침 시 posts 인식 못해서 고장나던거 수정

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -5,12 +5,13 @@ import {RootState} from "../store/store";
 import {removePost} from "../store/reducers/postReducer";
 import UserInformation from "../components/common/UserInformation";
 import {FaArrowLeft, FaPenSquare, FaTrash} from "react-icons/fa";
+import Loading from "../components/common/Loading";
 
 export default function Post() {
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
 	const { bid, id } = useParams();
-	const { uid, post, users } = useSelector((state: RootState) => ({ uid: state.auth.user?.uid, post: state.posts[bid!][id!], users: state.users }));
+	const { uid, post, users } = useSelector((state: RootState) => ({ uid: state.auth.user?.uid, post: Object.values(state.posts).length !== 0 ? state.posts[bid!][id!] : undefined, users: state.users }));
 
 	const handleRemove = () => {
 		dispatch(removePost({ bid, pid: id }));
@@ -19,26 +20,34 @@ export default function Post() {
 
 	return (
 		<div>
-			<div className="relative flex flex-col items-center mt-4 pb-2 mb-2 shadow-[0_5px_10px_-3px_#d0d0cf]">
-				<h2 className="text-xl font-bold">{post.title}</h2>
-				<UserInformation {...users[post.uid]} />
-				<div className="text-xs">
-					<div>생성일자: {post.createdAt}</div>
-					<div>수정일자: {post.updatedAt}</div>
-				</div>
-				<div className="absolute left-0 top-0 ml-2">
-					<Link to={`/board/${post.bid}`}><FaArrowLeft /></Link>
-				</div>
-				{
-					post.uid === uid && (
-						<div className="absolute right-0 top-1/2 -translate-y-2/4 flex flex-col gap-2 mr-2">
-							<Link to={`/post/edit/${post.bid}/${post.pid}`}><FaPenSquare /></Link>
-							<button onClick={handleRemove}><FaTrash /></button>
-						</div>
+			{
+				post
+					? (
+						<>
+							<div className="relative flex flex-col items-center mt-4 pb-2 mb-2 shadow-[0_5px_10px_-3px_#d0d0cf]">
+								<h2 className="text-xl font-bold">{post.title}</h2>
+								<UserInformation {...users[post.uid]} />
+								<div className="text-xs">
+									<div>생성일자: {post.createdAt}</div>
+									<div>수정일자: {post.updatedAt}</div>
+								</div>
+								<div className="absolute left-0 top-0 ml-2">
+									<Link to={`/board/${post.bid}`}><FaArrowLeft/></Link>
+								</div>
+								{
+									post.uid === uid && (
+										<div className="absolute right-0 top-1/2 -translate-y-2/4 flex flex-col gap-2 mr-2">
+											<Link to={`/post/edit/${post.bid}/${post.pid}`}><FaPenSquare/></Link>
+											<button onClick={handleRemove}><FaTrash/></button>
+										</div>
+									)
+								}
+							</div>
+							<p className="py-2 px-4 select-text whitespace-pre-line">{post.content}</p>
+						</>
 					)
-				}
-			</div>
-			<p className="py-2 px-4 select-text whitespace-pre-line">{post.content}</p>
+					: <Loading />
+			}
 		</div>
 	);
 }
