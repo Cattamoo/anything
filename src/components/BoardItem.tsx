a84340f1adecd2aad6926af6ac4940dca92c0741
Cattamoo <ouob77@kakao.com>
2023-05-25 12:16:29 +0900

    Home 화면 스타일링

--- src/components/BoardItem.tsx
+++ src/components/BoardItem.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import {useDispatch} from "react-redux";
 import {Board} from "../types/dataType";
 import {removeBoard} from "../store/reducers/boardReducer";
-import {Link} from "react-router-dom";
+import {BsTrash} from "react-icons/bs";
 
 type Props = {
 	board: Board;
@@ -11,19 +12,21 @@ type Props = {
 
 export default function BoardItem({ board, uid }: Props) {
 	const dispatch = useDispatch();
-	const buttonClassName = 'px-2 py-0.5 rounded';
 	const handleRemove = () => {
-		dispatch(removeBoard({ id: board.bid }))
+		dispatch(removeBoard({ id: board.bid }));
 	}
 	return (
-		<div className="flex items-center gap-2">
-			<Link to={`/board/${board.bid}`}>{board.title}</Link>
-			{uid && board.uid === uid && (
-				<>
-					<Link to={`/board/edit/${board.bid}`} className={`${buttonClassName} bg-blue-100 hover:bg-blue-200`}>수정</Link>
-					<button onClick={handleRemove} className={`${buttonClassName} bg-red-100 hover:bg-red-200`}>삭제</button>
-				</>
-			)}
-		</div>
+		<li className="w-full hover:bg-zinc-50 p-2">
+			<Link to={`/board/${board.bid}`} className="flex items-center gap-4">
+				<h2 className="text-xl font-bold">{board.title}</h2>
+				{
+					uid && board.uid === uid && (
+							<button onClick={handleRemove} title="삭제">
+								<BsTrash />
+							</button>
+					)
+				}
+			</Link>
+		</li>
 	);
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import {useSelector} from "react-redux";
 import {RootState} from "../store/reducers/reducers";
-import BoardItem from "../components/BoardItem";
 import PageLayout from "../components/layout/PageLayout";
+import BoardItem from "../components/BoardItem";
 
 export default function Home() {
 	const { user, boards } = useSelector((state: RootState) => ({ user: state.auth.user, boards: state.boards }));
 
 	return (
 		<PageLayout>
-			Home
-			{
-				user && Object.keys(boards)
-					.filter((key) => boards[key].isPublic || boards[key].user.includes(user.uid))
-					.map((key) => (
-						<BoardItem key={key} uid={user!.uid} board={boards[key]} />
-					))
-			}
+			<ul className="flex flex-col gap-2 mt-2">
+				{
+					user && (
+						Object.keys(boards)
+							.filter((key) => boards[key].isPublic || boards[key].user.includes(user.uid))
+							.map((key) => (
+								<BoardItem key={key} uid={user!.uid} board={boards[key]} />
+							))
+						)
+				}
+			</ul>
 		</PageLayout>
 	);
 }
