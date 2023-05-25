e128ba8bac9bbe57959a021525336ef6adc34512
Cattamoo <ouob77@kakao.com>
2023-05-25 12:24:19 +0900

    Board 화면 스타일링

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Link, useParams} from "react-router-dom";
 import {useSelector} from "react-redux";
+import moment from "moment/moment";
 import {RootState} from "../store/reducers/reducers";
+import PageLayout from "../components/layout/PageLayout";
 import Loading from "../components/common/Loading";
 import PostItem from "../components/PostItem";
-import {BsFilePlus} from "react-icons/bs";
-import moment from "moment/moment";
-import PageLayout from "../components/layout/PageLayout";
+import {BsFilePlus, BsPencil, BsPersonAdd} from "react-icons/bs";
 
 export default function Board() {
 	const { id } = useParams();
@@ -18,8 +18,20 @@ export default function Board() {
 				board
 					? (
 						<>
-							<h2 className="flex items-center text-xl font-bold gap-2 p-2">
+							<h2 className="flex items-center text-xl font-bold gap-2 py-2 px-1">
 								{ board.title }
+								{
+									uid === board.uid && (
+										<>
+											<Link to={`/board/edit/${board.bid}`} title="수정">
+												<BsPencil />
+											</Link>
+											<Link to={`/board/${board.bid}`} title="초대">
+												<BsPersonAdd />
+											</Link>
+										</>
+									)
+								}
 								{
 									uid && board.user.includes(uid) && (
 										<Link to={`/post/new/${board.bid}`} title="새 게시글">
