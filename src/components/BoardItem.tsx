b4c3a457b60a24a6270da2b2491c444e289c4bd0
Cattamoo <ouob77@kakao.com>
2023-05-25 17:26:45 +0900

    게시판에 게시판 주인 출력

--- src/components/BoardItem.tsx
+++ src/components/BoardItem.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../store/reducers/reducers";
 import {Board} from "../types/dataType";
 import {removeBoard} from "../store/reducers/boardReducer";
 import Title from "./ui/Title";
+import UserInformation from "./common/UserInformation";
 import {BsTrash} from "react-icons/bs";
 
 type Props = {
@@ -13,13 +15,15 @@ type Props = {
 
 export default function BoardItem({ board, uid }: Props) {
 	const dispatch = useDispatch();
+	const user = useSelector((state: RootState) => state.users[board.uid]);
 	const handleRemove = () => {
 		dispatch(removeBoard({ id: board.bid }));
 	}
 	return (
-		<li className="w-full flex items-center gap-4 hover:bg-zinc-50 p-2">
-			<Link to={`/board/${board.bid}`}>
+		<li className="w-full flex items-center justify-between gap-4 hover:bg-zinc-50 p-2">
+			<Link to={`/board/${board.bid}`} className="flex-1">
 				<Title>{board.title}</Title>
+				<UserInformation {...user} />
 			</Link>
 			{
 				uid && board.uid === uid && (
