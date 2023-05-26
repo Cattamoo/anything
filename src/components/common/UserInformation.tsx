34a03c1c46560aa8bb614aef26c9a832d21bd7bd
Cattamoo <ouob77@kakao.com>
2023-05-26 17:40:44 +0900

    이미지 이상하게 출력되는거 수정

--- src/components/common/UserInformation.tsx
+++ src/components/common/UserInformation.tsx
@@ -6,7 +6,7 @@ type Props = UserType;
 export default function UserInformation({ photoURL, email, displayName, nickname, profile }: Props) {
 	return (
 		<div className="flex items-center gap-2">
-			<img className="w-8 rounded-full" src={profile || photoURL} alt={email} />
+			<img className="w-8 aspect-square object-cover rounded-full" src={profile || photoURL} alt={email} />
 			<span>{nickname || displayName}</span>
 		</div>
 	);
